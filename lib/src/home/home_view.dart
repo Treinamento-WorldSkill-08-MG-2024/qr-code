df24f8667033d11f7d4005c6f18435b7bf202471
Gabriel-Spinola <gabrielspinola77@gmail.com>
2024-07-22 12:46:17 -0300

    init

---
+++ lib/src/home/home_view.dart
@@ -0,0 +1,75 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class HomeView extends StatefulWidget {
+  static const routeName = '/home';
+
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+
+  final _qrKey = GlobalKey(debugLabel: 'QR');
+  Barcode? _qrCodeResult;
+  QRViewController? _qrViewController;
+
+  @override
+  void reassemble() {
+    super.reassemble();
+
+    if (Platform.isAndroid) {
+      _qrViewController!.pauseCamera();
+    } else if (Platform.isIOS) {
+      _qrViewController!.resumeCamera();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 5,
+              child: QRView(
+                key: _qrKey,
+                onQRViewCreated: _onQrViewCreated,
+                overlay: QrScannerOverlayShape(),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Center(
+                child: (_qrCodeResult != null)
+                    ? Text(
+                        'Barcode Type: ${_qrCodeResult!.format.formatName}   Data: ${_qrCodeResult!.code}',
+                      )
+                    : const Text('Scan a code'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _onQrViewCreated(QRViewController controller) {
+    _qrViewController = controller;
+
+    controller.scannedDataStream.listen((scanData) {
+      setState(() => _qrCodeResult = scanData);
+    });
+  }
+
+  @override
+  void dispose() {
+    _qrViewController!.dispose();
+    super.dispose();
+  }
+}
